0548e38521b2bf50cf9c3711837c85321c326671
mormiddle <mormiddle@163.com>
2023-06-26 16:33:28 +0800

    修改_实现错位补帧

--- SerialPortForHeard/SerialPortDemo/cpp/common/Datamanager.cpp
+++ SerialPortForHeard/SerialPortDemo/cpp/common/Datamanager.cpp
@@ -1,4 +1,4 @@
-#include "Datamanager.h"
+    #include "Datamanager.h"
 
 DataManager::DataManager(QObject *parent) : QObject(parent)
 {
@@ -8,59 +8,66 @@ DataManager::DataManager(QObject *parent) : QObject(parent)
 void DataManager::saveData()
 {
     // 创建新的 SINGAL_SCAN_LINE 对象来存储分段后的数据
-        m_saveScanLines.cols = m_repeateScanLines.cols;
-        m_saveScanLines.repeateTimes = m_repeateScanLines.repeateTimes;
+    m_saveScanLines.cols = m_repeateScanLines.cols;
+    m_saveScanLines.repeateTimes = m_repeateScanLines.repeateTimes;
 
-        // 确定最大分段大小，用于后续的填充
-        int maxSegmentSize = 0;
+    //对后排探头进行错位
+    for (int i = 0; i < m_repeateScanLines.ChanelData.size(); i++) {
+        if (i % 2 == 0) { // 如果 i 是偶数
+            m_repeateScanLines.ChanelData[i].removeFirst();
+            m_repeateScanLines.ChanelData[i].append(0.0);
+        }
+    }
 
-        // 根据 repeateTimes 进行分段
-        for (int i = 0; i < m_repeateScanLines.repeateTimes.size() - 1; i++) {
-            // 获取当前段的开始和结束索引
-            int startIndex = m_repeateScanLines.repeateTimes[i];
-            int endIndex = m_repeateScanLines.repeateTimes[i + 1];
+    // 确定最大分段大小，用于后续的填充
+    int maxSegmentSize = 0;
 
-            // 检查索引是否超出 ChanelData 的范围
-            if (startIndex < 0 || endIndex > m_repeateScanLines.ChanelData[0].size()) {
-                qDebug() << "Index out of range!";
-                return;
-            }
+    // 根据 repeateTimes 进行分段
+    for (int i = 0; i < m_repeateScanLines.repeateTimes.size() - 1; i++) {
+        // 获取当前段的开始和结束索引
+        int startIndex = m_repeateScanLines.repeateTimes[i];
+        int endIndex = m_repeateScanLines.repeateTimes[i + 1];
 
-            // 对每一段进行处理
-
-            for (int j = 0; j < 10; j++) {
-                // 如果是偶数段，则进行反转
-                SINGAL_CHANEL_DATA segment;
-                if (i % 2 == 1) {
-                    for (int k = endIndex - 1; k >= startIndex; --k) {
-                        segment.push_back(m_repeateScanLines.ChanelData[j][k]);
-                    }
-                }
-                // 如果是奇数段，则直接添加
-                else {
-                    for (int k = startIndex; k < endIndex; ++k) {
-                        segment.push_back(m_repeateScanLines.ChanelData[j][k]);
-                    }
-                }
-
-
-                // 更新最大分段大小
-                if (segment.size() > maxSegmentSize) {
-                    maxSegmentSize = segment.size();
-                }
-
-                m_saveScanLines.ChanelData.push_back(segment);
-            }
-
-
+        // 检查索引是否超出 ChanelData 的范围
+        if (startIndex < 0 || endIndex > m_repeateScanLines.ChanelData[0].size()) {
+            qDebug() << "Index out of range!";
+            return;
         }
 
-        // 填充较短的分段，使其长度与最大分段相同
-        for (auto& segment : m_saveScanLines.ChanelData) {
-            while (segment.size() < maxSegmentSize) {
-                segment.push_back(0);
+        // 对每一段进行处理
+        for (int j = 0; j < 10; j++) {
+            // 如果是偶数段，则进行反转
+            SINGAL_CHANEL_DATA segment;
+            if (i % 2 == 1) {
+                for (int k = endIndex - 1; k >= startIndex; --k) {
+                    segment.push_back(m_repeateScanLines.ChanelData[j][k]);
+                }
             }
+            // 如果是奇数段，则直接添加
+            else {
+                for (int k = startIndex; k < endIndex; ++k) {
+                    segment.push_back(m_repeateScanLines.ChanelData[j][k]);
+                }
+            }
+
+
+            // 更新最大分段大小
+            if (segment.size() > maxSegmentSize) {
+                maxSegmentSize = segment.size();
+            }
+
+            m_saveScanLines.ChanelData.push_back(segment);
         }
+
+
+    }
+
+    // 填充较短的分段，使其长度与最大分段相同
+    for (auto& segment : m_saveScanLines.ChanelData) {
+        while (segment.size() < maxSegmentSize) {
+            segment.push_back(0);
+        }
+    }
 
 
 
